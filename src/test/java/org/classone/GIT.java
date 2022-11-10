c994705ad99b3f52855e79c120da3f1be1ef1a31
mani96980 <reachmani25595@gmail.com>
2022-11-10 14:16:08 +0530

    mnaicode

---
+++ src/test/java/org/classone/GIT.java
@@ -0,0 +1,26 @@
+package org.classone;
+
+public class GIT {
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	private void apple() {
+		System.out.println("apple");
+
+	}
+	private void samsung() {
+		System.out.println("samsung");
+		
+	
+
+	}
+	private void oneplus() {
+		System.out.println("oneplus");
+
+	}
+
+
+
+}
